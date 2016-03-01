59443eacb8de078cc873e2eab063194fd3fc5982
Nicky Casey <caseyn9@tcd.ie>
2016-03-01 17:29:17 +0000

    Set a gps to get new location every 300 seconds. Added a gpsObserver class to log any print any changes made to location database to a log.

--- app/src/main/java/com/aware/plugin/template/Plugin.java
+++ app/src/main/java/com/aware/plugin/template/Plugin.java
@@ -1,28 +1,49 @@
 package com.aware.plugin.template;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.util.Log;
 
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
+import com.aware.providers.Locations_Provider;
 import com.aware.utils.Aware_Plugin;
 
 public class Plugin extends Aware_Plugin {
 
+    GpsObserver gpsO;
     @Override
     public void onCreate() {
         super.onCreate();
 
         TAG = "AWARE::"+getResources().getString(R.string.app_name);
         DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
+        if(DEBUG)
+            Log.d("Begin", "Group 25 pluggin running");
 
         //Initialize our plugin's settings
         if( Aware.getSetting(this, Settings.STATUS_PLUGIN_TEMPLATE).length() == 0 ) {
             Aware.setSetting(this, Settings.STATUS_PLUGIN_TEMPLATE, true);
         }
 
+        //some temporary variables
+        int gpsFrequency = 300;
+        int gpsAccuracy = 50;
+        int locationExpTime = 300;
+
         //Activate programmatically any sensors/plugins you need here
         //e.g., Aware.setSetting(this, Aware_Preferences.STATUS_ACCELEROMETER,true);
         //NOTE: if using plugin with dashboard, you can specify the sensors you'll use there.
+        //GPS sensor:
+        Aware.setSetting(this,Aware_Preferences.STATUS_LOCATION_GPS, true);
+        Aware.setSetting(this,Aware_Preferences.FREQUENCY_LOCATION_GPS, gpsFrequency);
+        Aware.setSetting(this,Aware_Preferences.MIN_LOCATION_GPS_ACCURACY, gpsAccuracy);
+        Aware.setSetting(this, Aware_Preferences.LOCATION_EXPIRATION_TIME, locationExpTime);
+        Aware.startSensor(this, Aware_Preferences.STATUS_LOCATION_GPS);
+
+        gpsO = new GpsObserver(new Handler(),this);    //two import options for 'Handler', I went with 'android.os.Handler'.
+        getContentResolver().registerContentObserver(Locations_Provider.Locations_Data.CONTENT_URI, true, gpsO);
+
 
         //Any active plugin/sensor shares its overall context using broadcasts
         CONTEXT_PRODUCER = new ContextProducer() {
@@ -44,6 +65,7 @@ public class Plugin extends Aware_Plugin {
         Aware.startPlugin(this, "com.aware.plugin.template");
     }
 
+
     //This function gets called every 5 minutes by AWARE to make sure this plugin is still running.
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -59,11 +81,15 @@ public class Plugin extends Aware_Plugin {
         super.onDestroy();
 
         Aware.setSetting(this, Settings.STATUS_PLUGIN_TEMPLATE, false);
+        getContentResolver().unregisterContentObserver(gpsO);
 
         //Deactivate any sensors/plugins you activated here
         //e.g., Aware.setSetting(this, Aware_Preferences.STATUS_ACCELEROMETER, false);
-
+        Aware.setSetting(this,Aware_Preferences.STATUS_LOCATION_GPS, false);
         //Stop plugin
         Aware.stopPlugin(this, "com.aware.plugin.template");
     }
+
 }
+
+
